46f241fa594ae9eaabff86cbd13a25ae5df79654
Valimp <valentinchauvet.contact@gmail.com>
2025-12-18 14:34:12 +0100

    ui: apartment section for dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="p-10">
+  return <div className="p-10 flex flex-col gap-8">
         { children }
     </div>;
 }
